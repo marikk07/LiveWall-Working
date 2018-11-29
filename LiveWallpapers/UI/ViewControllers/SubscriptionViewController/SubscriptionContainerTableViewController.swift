747d7e01ebd877bf781c53a30e40025e0acaba76
Maryan Pasichniak <>
2018-11-29 20:03:11 +0200

    localize price

--- LiveWallpapers/UI/ViewControllers/SubscriptionViewController/SubscriptionContainerTableViewController.swift
+++ LiveWallpapers/UI/ViewControllers/SubscriptionViewController/SubscriptionContainerTableViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class SubscriptionContainerTableViewController: UITableViewController {
-
+    
+    private var subscriptionPrice: String = "$7.99" //407080 id
+    private var monthlySubscriptionID: String = "$19.99" //203040 id
+    private var yearlySubscriptionID: String = "$49.99" //507080 id
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkLocalisePrice()
+    }
+    
+    // MARK: - Private Methods
+    
+    func checkLocalisePrice() {
+        SwiftyStoreKit.retrieveProductsInfo([Constants.subscriptionID, Constants.monthlySubscriptionID, Constants.yearlySubscriptionID]) { result in
+            for product in result.retrievedProducts {
+                if product.productIdentifier == Constants.subscriptionID {
+                    self.subscriptionPrice = product.localizedPrice ?? "$7.99"
+                } else if product.productIdentifier == Constants.monthlySubscriptionID {
+                    self.monthlySubscriptionID = product.localizedPrice ?? "$19.99"
+                } else if product.productIdentifier == Constants.yearlySubscriptionID {
+                    self.yearlySubscriptionID = product.localizedPrice ?? "$49.99"
+                }
+            }
+            self.priceWeekLabel.text = "then " + self.subscriptionPrice + " per week"
+            self.monthButtonPrice.text = self.monthlySubscriptionID
+            self.yearButtonPrice.text = self.yearlySubscriptionID
+            self.priceAsLowLabel.text = "Price: as low as " + self.subscriptionPrice + " per week."
+        }
+    }
+    
     // MARK: - IBOutlets
     
     @IBOutlet weak var weekView: UIView! {
@@ -24,6 +53,12 @@ class SubscriptionContainerTableViewController: UITableViewController {
             yearView.layer.borderColor = UIColor(hex: 0x00BFA5, alpha: 1).cgColor
         }
     }
+    
+    @IBOutlet weak var priceWeekLabel: UILabel!
+    @IBOutlet weak var monthButtonPrice: UILabel!
+    @IBOutlet weak var yearButtonPrice: UILabel!
+    @IBOutlet weak var priceAsLowLabel: UILabel!
+    
     
     // MARK: - IBActions
     
